703887c994de868ee652171f0337fe196571ab29
Guilherme Balzana <gb637@cam.ac.uk>
2020-01-28 17:19:12 +0000

    Add repository support

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,16 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
+url = "https://downloads.whamcloud.com/public/lustre/latest-release/el7/client"
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_lustre_repo(host):
+    repo = host.file('/etc/yum.repos.d/lustre-client.repo')
+    assert repo.exists
+    assert repo.contains(r'\[lustre-client\]')
+    assert repo.contains(f"baseurl = {url}")
