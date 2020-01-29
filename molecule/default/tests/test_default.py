a2067c81f33d3d9066b1b979ce7f1abb7fbdbf9d
Guilherme Balzana <gb637@cam.ac.uk>
2020-01-29 11:54:42 +0000

    Add support for multiple lustre mount points
    * Remove mount target dir creation since mount module already does that
    * Avoid modprobing when using docker
    * Fix typo on mount state

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,3 +14,18 @@ def test_lustre_repo(host):
     assert repo.exists
     assert repo.contains(r'\[lustre-client\]')
     assert repo.contains(f"baseurl = {url}")
+
+
+def test_lustre_package(host):
+    assert host.package('lustre-client').is_installed
+
+
+def test_lustre_mounts(host):
+    fstab = '/etc/fstab'
+    assert host.file(fstab).contains(
+        r'/default lustre _netdev,noatime,localflock,noauto '
+    )
+    assert host.file(fstab).contains(
+        r'/merge lustre _netdev,noatime,localflock,noauto,my_opt '
+    )
+    assert host.file(fstab).contains(r'/my lustre my_opt ')
